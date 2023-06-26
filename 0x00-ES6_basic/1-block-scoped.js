6366d9bc1d669cb43b903f31c148c90022f7bcdd
JosiahMbithi001 <mbithishucks@gmail.com>
2023-06-26 11:49:39 -0700

    1-blocked-scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-var */
 
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
