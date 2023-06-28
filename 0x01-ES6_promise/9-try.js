951ea35c064afb9de789fb5faad48ce7da89e384
JosiahMbithi001 <mbithishucks@gmail.com>
2023-06-28 11:14:39 -0700

    Throws an Erro

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queu = [];
+  const message = 'Guardrail was processed';
+  try {
+    const output = mathFunction();
+    queu.push(output);
+    queu.push(message);
+  } catch (err) {
+    queu.push(`Error ${err.message}`);
+    queu.push(message);
+  }
+  return queu;
+}
