18db4a75c1a7de0e484f6a82d5045cc96f068901
JosiahMbithi001 <mbithishucks@gmail.com>
2023-06-29 12:15:01 -0700

    Fixed ESLint

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,18 +1,18 @@
 export default class Airport {
-    constructor(name, code) {
-       if(typeof name === 'string'){
-            this._name = name
-       } else {
-            throw new TypeError('name is not a string')
-       }
-       if(typeof code === 'string'){
-            this._code = code
-       } else {
-            throw new TypeError('code is not string')
-       }
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('name is not a string');
     }
-    toString() {
-        return (`[object ${this._code}]`)
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('code is not string');
     }
+  }
 
+  toString() {
+    return (`[object ${this._code}]`);
+  }
 }
