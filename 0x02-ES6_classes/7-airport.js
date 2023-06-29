1c6c96032f7accf66add0864f7e94a0599937135
JosiahMbithi001 <mbithishucks@gmail.com>
2023-06-29 10:57:27 -0700

    Holberton Class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport {
+    constructor(name, code) {
+       if(typeof name === 'string'){
+            this._name = name
+       } else {
+            throw new TypeError('name is not a string')
+       }
+       if(typeof code === 'string'){
+            this._code = code
+       } else {
+            throw new TypeError('code is not string')
+       }
+    }
+    toString() {
+        return (`object ${}`)
+    }
+
+}
