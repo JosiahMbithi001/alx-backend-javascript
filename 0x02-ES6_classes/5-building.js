0e907727846143acec9ca800ff7f682fa3d7e130
JosiahMbithi001 <mbithishucks@gmail.com>
2023-06-29 06:07:03 -0700

    ESLint Fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,16 @@
-export default class Building{
-    constructor(sqft){
-        if(new.target !== Building && this.evacuationWarningMessage === undefined){
-        throw new Error('Class extending Building must override evacuationWarningMessage')
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-       if (typeof sqft === 'number'){
-            this._sqft = sqft
-        } else {
-            throw new Error('sqft is not a number')
-        }
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new Error('sqft is not a number');
     }
-    get sqft(){
-        return this._sqft;
-    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
