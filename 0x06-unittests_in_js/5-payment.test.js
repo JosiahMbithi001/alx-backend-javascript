a0406dbe86d8801e4782fe492b57c4c2ccc5d8bf
JosiahMbithi001 <mbithishucks@gmail.com>
2023-08-25 00:06:29 -0700

    Using Hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,32 +1,29 @@
-// 5-payment.test.js
 const sinon = require('sinon');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
-const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-    let calculateNumberStub;
-    let consoleSpy;
+  let newEmployee;
 
-    beforeEach(() => {
-        calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-        consoleSpy = sinon.spy(console, 'log');
-    });
+  beforeEach(() => {
+    if (!newEmployee) {
+      newEmployee = sinon.spy(console);
+    }
+  });
 
-    afterEach(() => {
-        calculateNumberStub.restore();
-        consoleSpy.restore();
-    });
+  afterEach(() => {
+    newEmployee.log.resetHistory();
+  });
 
-    it('should log the correct message with the sum of 100 and 20', () => {
-        sendPaymentRequestToApi(100, 20);
-        sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
-        sinon.assert.calledOnce(consoleSpy);
-    });
+  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(newEmployee.log.calledWith('The total is: 120')).to.be.true;
+    expect(newEmployee.log.calledOnce).to.be.true;
+  });
 
-    it('should log the correct message with the sum of 10 and 10', () => {
-        sendPaymentRequestToApi(10, 10);
-        sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
-        sinon.assert.calledOnce(consoleSpy);
-    });
+  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(newEmployee.log.calledWith('The total is: 20')).to.be.true;
+    expect(newEmployee.log.calledOnce).to.be.true;
+  });
 });
-
